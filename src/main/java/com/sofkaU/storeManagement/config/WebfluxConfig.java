eba68ad364ce471c16e5d83672def41ee0fcd65c
Manuel Pineda <pineda.manuel@gmail.com>
2022-06-13 12:13:48 -0500

    EnableWebFlux included

--- src/main/java/com/sofkaU/storeManagement/config/WebfluxConfig.java
+++ src/main/java/com/sofkaU/storeManagement/config/WebfluxConfig.java
@@ -3,10 +3,12 @@ package com.sofkaU.storeManagement.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
 @Configuration
+@EnableWebFlux
 public class WebfluxConfig {
 
     @Bean
